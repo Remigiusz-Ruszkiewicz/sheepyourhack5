e9085c2cb22a4c76693ab417d51789df19e8d70a
k_domon <k.domon@dasl.pl>
2023-12-16 04:02:12 +0100

    draggable list

--- apps/harmony_app/lib/src/home_page/home_page_view.dart
+++ apps/harmony_app/lib/src/home_page/home_page_view.dart
@@ -1,10 +1,11 @@
 import 'package:async_builder/async_builder.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get_it/get_it.dart';
 import 'package:harmony_app/src/activity_list/activity_list_view.dart';
 import 'package:harmony_app/src/home_page/home_page_bloc.dart';
-import 'package:harmony_app/src/progress_list/progress_list_view.dart';
 import 'package:harmony_app/src/models/task.dart';
+import 'package:harmony_app/src/progress_list/progress_list_view.dart';
 import 'package:harmony_app/src/widgets/animated_scale.dart';
 import 'package:harmony_app/src/widgets/dragging_list_item.dart';
 import 'package:harmony_app/src/widgets/menu_list_item.dart';
@@ -20,15 +21,26 @@ class HomePageView extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocProvider<HomePageBloc>(
       instance: HomePageBloc.new,
-      child: Scaffold(
-        appBar: AppBar(
-          actions: const [
-            _MenuButton(),
-          ],
+      child: Container(
+        constraints: const BoxConstraints.expand(),
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/background.png'),
+            fit: BoxFit.cover,
+          ),
         ),
-        body: const SingleChildScrollView(
-          padding: EdgeInsets.symmetric(horizontal: 16),
-          child: _PageBody(),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          appBar: AppBar(
+            backgroundColor: Colors.transparent,
+            actions: const [
+              _MenuButton(),
+            ],
+          ),
+          body: const SingleChildScrollView(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            child: _PageBody(),
+          ),
         ),
       ),
     );
@@ -50,7 +62,10 @@ class _PageBody extends StatelessWidget {
           initial: '',
           builder: (_, time) => Text(
             time!,
-            style: const TextStyle(fontSize: 36),
+            style: const TextStyle(
+              fontSize: 36,
+              color: Colors.black,
+            ),
           ),
         ),
         AsyncBuilder<String>(
@@ -58,7 +73,10 @@ class _PageBody extends StatelessWidget {
           retain: true,
           builder: (_, hint) => Text(
             hint!,
-            style: const TextStyle(fontSize: 24),
+            style: const TextStyle(
+              fontSize: 24,
+              color: Colors.black,
+            ),
           ),
         ),
         _DraggableList(bloc),
@@ -81,58 +99,52 @@ class _DraggableListState extends State<_DraggableList> with TickerProviderState
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
+    return Column(
       children: [
-        SafeArea(
-          child: Column(
-            children: [
-              Container(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 8,
-                  vertical: 20,
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 6),
-                  child: DragTarget<Task>(
-                    builder: (context, candidateItems, rejectedItems) {
-                      return const ScaleWidget();
-                    },
-                    onAccept: (item) {
-                      setState(() {
-                        widget.bloc.setCompleted(item);
-                      });
-                    },
-                  ),
-                ),
-              ),
-              AsyncBuilder<List<Task>>(
-                stream: widget.bloc.tasksStream,
-                retain: true,
-                builder: (context, tasks) {
-                  return ListView.separated(
-                    padding: EdgeInsets.zero,
-                    itemCount: tasks!.length,
-                    shrinkWrap: true,
-                    separatorBuilder: (context, index) {
-                      return const SizedBox(height: 12);
-                    },
-                    itemBuilder: (context, index) {
-                      final task = tasks[index];
-                      return LongPressDraggable<Task>(
-                        data: task,
-                        dragAnchorStrategy: pointerDragAnchorStrategy,
-                        feedback: DraggingListItem(
-                          dragKey: _draggableKey,
-                          task: task,
-                        ),
-                        child: MenuListItem(task: task),
-                      );
-                    },
-                  );
-                },
-              ),
-            ],
+        Container(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 8,
+            vertical: 20,
           ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 6),
+            child: DragTarget<Task>(
+              builder: (context, candidateItems, rejectedItems) {
+                return const ScaleWidget();
+              },
+              onAccept: (item) {
+                setState(() {
+                  widget.bloc.setCompleted(item);
+                });
+              },
+            ),
+          ),
+        ),
+        AsyncBuilder<List<Task>>(
+          stream: widget.bloc.tasksStream,
+          retain: true,
+          builder: (context, tasks) {
+            return ListView.separated(
+              padding: EdgeInsets.zero,
+              itemCount: tasks!.length,
+              shrinkWrap: true,
+              separatorBuilder: (context, index) {
+                return const SizedBox(height: 12);
+              },
+              itemBuilder: (context, index) {
+                final task = tasks[index];
+                return LongPressDraggable<Task>(
+                  data: task,
+                  dragAnchorStrategy: pointerDragAnchorStrategy,
+                  feedback: DraggingListItem(
+                    dragKey: _draggableKey,
+                    task: task,
+                  ),
+                  child: MenuListItem(task: task),
+                );
+              },
+            );
+          },
         ),
       ],
     );
@@ -155,29 +167,26 @@ class _MenuButton extends StatelessWidget {
               controller.open();
             }
           },
-          icon: const Icon(Icons.menu),
+          icon: const Icon(
+            Icons.menu,
+            color: Colors.black,
+          ),
         );
       },
       menuChildren: [
         MenuItemButton(
           leadingIcon: const Icon(Icons.info_outline),
-          child: Text(LocaleKeys.about.tr()),
-          onPressed: () async {},
+          child: const Text('Activity List'),
+          onPressed: () {
+            Navigator.of(context).push<void>(MaterialPageRoute(builder: (_) => const ActivityListView()));
+          },
         ),
         MenuItemButton(
           leadingIcon: const Icon(Icons.feed_outlined),
-          child: Text(LocaleKeys.logs.tr()),
-          onPressed: () {},
-        ),
-        MenuItemButton(
-          leadingIcon: const Icon(Icons.settings),
-          child: Text(LocaleKeys.settings.tr()),
-          onPressed: () {},
-        ),
-        MenuItemButton(
-          leadingIcon: const Icon(Icons.help_outline),
-          child: Text(LocaleKeys.help.tr()),
-          onPressed: () {},
+          child: const Text('Progress'),
+          onPressed: () {
+            Navigator.of(context).push<void>(MaterialPageRoute(builder: (_) => const ProgressListView()));
+          },
         ),
       ],
     );
